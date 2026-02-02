859001b389f63e8f6ca3e2ae77517ab3f4ed11bf
Aleks Rutins <keeper@farthergate.com>
2026-02-01 21:44:48 -0500

    Fix frontend error handling, use taskfile

--- public/js/api.js
+++ public/js/api.js
@@ -8,6 +8,7 @@ export async function apiQuery(language, query) {
     body: query,
   });
   const qres = await res.json();
+  if ("error" in qres) return qres;
   const obj = Object.fromEntries(
     qres.values.map((val) => val.map((v, idx) => [qres.columns[idx], v])),
   );
