980a91add479a5e98e01695b992105c21c145f02
Aleks Rutins <keeper@farthergate.com>
2026-02-01 21:33:15 -0500

    Fix frontend for new query format

--- public/js/api.js
+++ public/js/api.js
@@ -1,11 +1,15 @@
 export async function apiQuery(language, query) {
-    const res = await fetch(`/api/query?language=${language}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "text/plain",
-            "Accept": "application/json"
-        },
-        body: query
-    });
-    return await res.json();
+  const res = await fetch(`/api/query?language=${language}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain",
+      Accept: "application/json",
+    },
+    body: query,
+  });
+  const qres = await res.json();
+  const obj = Object.fromEntries(
+    qres.values.map((val) => val.map((v, idx) => [qres.columns[idx], v])),
+  );
+  return obj;
 }
